820ad6b6b949505e4f530eade75c5663a03146f8
Aurimas <snoutupgames@gmail.com>
2018-08-05 19:37:33 +0300

    Added jokes

--- JokeLib/src/main/java/com/udacity/garuolis/jokelib/JokeTeller.java
+++ JokeLib/src/main/java/com/udacity/garuolis/jokelib/JokeTeller.java
@@ -1,10 +1,18 @@
 package com.udacity.garuolis.jokelib;
 
-public class JokeTeller {
+import java.util.Random;
 
+public class JokeTeller {
+    static Random random = new Random();
 
     public static String TellMeSomeJoke() {
-        return "This is a funny joke. Please, laugh!";
+        String[] jokes = new String[5];
+        jokes[0] = "Did you hear about the crook who stole a calendar? He got twelve months.";
+        jokes[1] = "Q. What’s the difference between ignorance and apathy? A. I don’t know and I don’t care.";
+        jokes[2] = "I woke up this morning and forgot which side the sun rises from, then it dawned on me.";
+        jokes[3] = "I own the world’s worst thesaurus. Not only is it awful, it’s awful.";
+        jokes[4] = "I hate Russian dolls, they’re so full of themselves.";
+        return jokes[random.nextInt(jokes.length)];
     }
 }
 
